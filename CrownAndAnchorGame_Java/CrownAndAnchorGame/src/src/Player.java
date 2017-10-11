439b573ce0168142342138015290476f5d55a700
Tejaschokshi <chokshitejas@gmail.com>
2017-10-11 13:52:03 +1100

    testing the bug 1 game limit

--- CrownAndAnchorGame_Java/CrownAndAnchorGame/src/src/Player.java
+++ CrownAndAnchorGame_Java/CrownAndAnchorGame/src/src/Player.java
@@ -18,9 +18,9 @@ public class Player {
 	public int getLimit() { return limit; }
 	
 	public void setLimit(int limit) {
-		if (limit < 0) throw new IllegalArgumentException
+		if (limit <= 0) throw new IllegalArgumentException
 		("Limit cannot be negative.");
-		if (limit > balance)  throw new IllegalArgumentException("Limit cannot be greater than balance.");
+		if (limit => balance)  throw new IllegalArgumentException("Limit cannot be greater than balance.");
 		this.limit = limit;
 	}
 
