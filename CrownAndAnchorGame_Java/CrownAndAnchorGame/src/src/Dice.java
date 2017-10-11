d9242a7db8fd328ff8b3fcd39c85464813e914c7
Tejaschokshi <chokshitejas@gmail.com>
2017-10-11 15:13:56 +1100

    bug 3 is identified

--- CrownAndAnchorGame_Java/CrownAndAnchorGame/src/src/Dice.java
+++ CrownAndAnchorGame_Java/CrownAndAnchorGame/src/src/Dice.java
@@ -9,7 +9,8 @@ public class Dice {
 	public DiceValue getValue() {
 		return value;
 	}
-
+// All rolls are the same in each run through 
+//bug 3 is identified 
 	public DiceValue roll() {
 		return DiceValue.getRandom();
 	}		
